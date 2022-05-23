153f4e1ec43f4c4954565f538aae57b3c23bfeaf
joelgroomer <joelgroomer@users.noreply.github.com>
2022-05-22 18:10:05 -0700

    Added Card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Nav/Header';
+import Card from './components/UI/Card';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Card>
+        <h2>Testing</h2>
+        <p>Here is a test card.</p>
+      </Card>
     </div>
   );
 }
