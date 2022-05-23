2bdd0f25573a5cbcd1717cfda6a1bed1fd0cacfc
joelgroomer <joelgroomer@users.noreply.github.com>
2022-05-22 19:32:43 -0700

    Added and styled Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Header from './components/Nav/Header';
 import Card from './components/UI/Card';
+import Jumbotron from './components/UI/Jumbotron';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Jumbotron
+        image={process.env.PUBLIC_URL + '/food.jpg'}
+        className="welcome"
+      >
+        <h1 style={{ textAlign: 'center' }}>
+          Delicious Food, Delivered to You
+        </h1>
+        <p>
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home.
+        </p>
+        <p>
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and of course by experienced chefs!
+        </p>
+      </Jumbotron>
       <Card>
         <h2>Testing</h2>
         <p>Here is a test card.</p>
--- src/components/UI/Jumbotron.js
+++ src/components/UI/Jumbotron.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import Card from './Card';
+import styles from './Jumbotron.module.css';
 
-const Jumbotron = props => {};
+const Jumbotron = props => {
+  return (
+    <div
+      className={
+        styles.jumbotron + (props.className ? ` ${props.className}` : '')
+      }
+    >
+      <div className={styles['jumbotron-img-container']}>
+        <img
+          src={props.image}
+          className={styles['jumbotron-img']}
+          alt={props.imageAlt || ''}
+        />
+      </div>
+      <Card className={styles['jumbotron-card']}>{props.children}</Card>
+    </div>
+  );
+};
 
 export default Jumbotron;
