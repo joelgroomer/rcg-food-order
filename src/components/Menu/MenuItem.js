57c6df7f12be8271c4a5425ce4e639a2afbe64e0
joelgroomer <joelgroomer@users.noreply.github.com>
2022-05-25 19:50:28 -0700

    Added styling for MenuItem

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -9,20 +9,25 @@ const MenuItem = props => {
     setQty(event.target.value);
   };
 
+  const handleClick = event => {
+    event.preventDefault();
+    props.addItem(props.item, qty);
+  };
+
   return (
     <div className={styles['menu-item']}>
       <div className={styles.details}>
         <h3 className={styles['item-name']}>{props.item.name}</h3>
-        <p>{props.item.description}</p>
+        <h3 className={styles.description}>{props.item.description}</h3>
         <h3 className={styles.price}>{props.item.price}</h3>
       </div>
-      <div className={styles.controls}>
-        <label htmlFor="qty"></label>
+      <form className={styles.controls}>
+        <label htmlFor="qty">Amount</label>
         <input id="qty" type="number" onChange={onQtyChange} value={qty} />
-        <Button onClick={props.addItem.bind(this, props.item, qty)}>
+        <Button fill="true" onClick={handleClick}>
           + Add
         </Button>
-      </div>
+      </form>
     </div>
   );
 };
